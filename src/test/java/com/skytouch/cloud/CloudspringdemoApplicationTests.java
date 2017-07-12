1d04b2c3610a32891e3a5b1ab0df14cd9f5474d6
Antuan Yanez Villa <ayanezvilla@atlassian.com>
2017-07-12 10:28:17 -0600

    configuration server skt exam

---
+++ src/test/java/com/skytouch/cloud/CloudspringdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skytouch.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudspringdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
